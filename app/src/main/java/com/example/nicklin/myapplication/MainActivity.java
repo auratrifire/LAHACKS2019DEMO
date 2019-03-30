2f3ffcdf77a8940fd8c86b5a647b28f69349fe6d
Cody Fan <CodyCeilingfan@gmail.com>
2019-03-30 11:32:10 -0700

    Started to implement the Android Camera API.

--- app/src/main/java/com/example/nicklin/myapplication/MainActivity.java
+++ app/src/main/java/com/example/nicklin/myapplication/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.nicklin.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    Camera camera;
+    FrameLayout frameLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        frameLayout = (FrameLayout)findViewById(R.id.frameLayout);
+
+        camera = Camera.open();
     }
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
+
+
 }
